2b57445708abc32a327494a7cc27117198f8db35
xTCry [Vladislav Kh] <wolt6@mail.ru>
2024-03-06 03:46:38 +0300

    fix: val resourcePath not empty

--- src/loaders/CustomLoader.ts
+++ src/loaders/CustomLoader.ts
@@ -52,7 +52,7 @@ export default class CustomLoader extends LoaderClass {
         this.otherCSS = css;
 
         if (this.resourcePath === true) {
-            this.resourcePath = staticName;
+            this.resourcePath = staticName || './';
         }
     }
 
